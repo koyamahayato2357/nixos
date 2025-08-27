a54e348aaf9ccc6a7993f2fd8c48154dc38e7fd6
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2025-08-27 20:33:45 +0900

    add clipboard

--- configuration.nix
+++ configuration.nix
@@ -128,6 +128,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     vulkan-loader
     vulkan-tools
+    wl-clipboard
     wget
   ];
 
