f6383e9ca42d99d9168d3bb59b81f7d078ea8299
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2025-08-27 20:32:13 +0900

    add vulkan

--- configuration.nix
+++ configuration.nix
@@ -126,6 +126,8 @@
   environment.systemPackages = with pkgs; [
     fastfetch
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vulkan-loader
+    vulkan-tools
     wget
   ];
 
