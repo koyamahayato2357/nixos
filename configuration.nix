e989c3cde6b6f4817f8a13f133b28020f7ba676e
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2025-09-06 23:56:08 +0900

    add bash

--- configuration.nix
+++ configuration.nix
@@ -121,6 +121,7 @@
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
+    bash
     fastfetch
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     vulkan-loader
