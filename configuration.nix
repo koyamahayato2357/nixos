2f1381178c34dfd2eb2cea17aebc57bf9f9da05b
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2025-09-06 16:08:07 +0900

    disable boot menu

--- configuration.nix
+++ configuration.nix
@@ -15,7 +15,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.configurationLimit = 5;
+  boot.loader.timeout = 0;
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
